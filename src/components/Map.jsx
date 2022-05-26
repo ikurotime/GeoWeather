d77c7ced3df95df4217cd56d54f461b4a9061b7c
ikurotime <davidhuertasortiz@gmail.com>
2022-05-26 02:44:27 +0200

    Add(Map)

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,19 +1,30 @@
-import { memo, useContext } from 'react'
+import { memo, useContext, useEffect, useState } from 'react'
 
 import { GoogleMap } from '@react-google-maps/api'
 import { WeatherContext } from '../context/WeatherContextProvider'
 
 function Map({ className }) {
-  const { location } = useContext(WeatherContext)
-  const center = {
-    lat: location.lat,
-    lng: location.lon
+  const { location, searchWeatherByLatLng } = useContext(WeatherContext)
+  const [center, setCenter] = useState({ lat: 0, lng: 0 })
+
+  useEffect(() => {
+    if (location) {
+      setCenter({ lat: location.lat, lng: location.lon })
+    }
+  }, [location])
+  const handleClick = (n) => {
+    searchWeatherByLatLng({
+      lat: n.latLng.lat(),
+      lng: n.latLng.lng()
+    })
   }
   return (
-    <GoogleMap mapContainerClassName={className} center={center} zoom={10}>
-      {/* Child components, such as markers, info windows, etc. */}
-      <></>
-    </GoogleMap>
+    <GoogleMap
+      mapContainerClassName={className}
+      center={center}
+      zoom={10}
+      onClick={handleClick}
+    ></GoogleMap>
   )
 }
 
