41b2ff3cb714d365652c7723f83dd9b41aa8a721
ikurotime <davidhuertasortiz@gmail.com>
2022-05-27 01:14:32 +0200

    Change(MainPanel name)

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,13 @@
 import Footer from './components/Footer'
 import Header from './components/Header'
-import LeftPanel from './components/LeftPanel'
+import MainPanel from './components/MainPanel'
 import WeatherContextProvider from './context/WeatherContextProvider'
 
 function App() {
   return (
     <WeatherContextProvider>
       <Header />
-      <div className='grid justify-center min-h-screen gap-3 p-8 bg-white'>
-        <LeftPanel />
-      </div>
+      <MainPanel />
       <Footer />
     </WeatherContextProvider>
   )
