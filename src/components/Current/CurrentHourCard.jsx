1eb0d3034223e4305a67c4da522a75efcae357ef
Carlos Fernandez Cabrero <carlitostrabajo2018@outlook.es>
2023-08-13 19:30:06 +0200

    modifico la altura de la tarjeta

--- src/components/Current/CurrentHourCard.jsx
+++ src/components/Current/CurrentHourCard.jsx
@@ -6,7 +6,7 @@ import { useContext } from 'react'
 export default function CurrentHourCard({ element }) {
   const { degreeType } = useContext(WeatherContext)
   return (
-    <div className='w-28 h-44 mt-5 mr-1 inline-block border-[3px] bg-white dark:bg-slate-700 dark:border-slate-900 text-center'>
+    <div className='w-28 h-48 mt-5 mr-1 inline-block border-[3px] bg-white dark:bg-slate-700 dark:border-slate-900 text-center'>
       <div className='grid place-content-center place-items-center h-full '>
         <Img
           className='mt-1'
@@ -26,7 +26,7 @@ export default function CurrentHourCard({ element }) {
         <p className='dark:text-white font-bold text-base'>
           {degreeType === 'C' ? element.temp_c + 'ºC' : element.temp_f + 'ºF'}
         </p>
-        <p className='dark:text-white font-bold text-sm whitespace-pre-line h-10'>
+        <p className='dark:text-white font-bold text-sm whitespace-pre-line h-16'>
           {element.condition?.text}
         </p>
       </div>
